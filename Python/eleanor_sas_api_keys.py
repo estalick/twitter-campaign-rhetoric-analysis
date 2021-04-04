1fd0e500d1629a3084b2b9fc2e67fb5a649d570f
estalick <egstalick@gmail.com>
2021-04-04 17:26:34 -0400

    Delete eleanor_sas_api_keys.py

--- Python/eleanor_sas_api_keys.py
+++
@@ -1,6 +0,0 @@
-eleanor_sas_twitter_credentials = {
-    "consumer_key": "j2fKErxzJgDhjWTEJaaaR9Z6N",
-    "consumer_secret": "1iKtLTA86Grxcc30MdDqDtjgqoaJCxfhs7iDdnTIhspbxNEwmQ",
-    "access_token": "1363234169099223040-jSLRVfABYghOoxrdSMBpEq4aFuYbOZ",
-    "access_token_secret": "OiI5IwA6bgec9sVUAvVLp5mRUslO4nnJh9g1RUA0okW31"
-}
